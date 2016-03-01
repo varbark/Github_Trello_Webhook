f328e917ca946048c41fed18d85420ee1e5dc0de
varbark <naoki3355@gmail.com>
2016-03-01 16:25:46 +0800

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '162.243.121.32', user: 'deploy', roles: %w{app db web}
+server '162.243.102.166', user: 'deploy', roles: %w{app db web}
 set :unicorn_rack_env, -> { "production" }
 
 
